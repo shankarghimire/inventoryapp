1f87678b651e05e76c61bf5846807e3401e68530
Shankar Ghimire <ssghimire@gmail.com>
2020-10-03 12:14:02 -0400

    Adds Edit profuct feature

--- src/main/java/com/sheridancollege/ghimirsh/springbootinventoryapp/controllers/HomeController.java
+++ src/main/java/com/sheridancollege/ghimirsh/springbootinventoryapp/controllers/HomeController.java
@@ -6,7 +6,11 @@ import org.hibernate.metamodel.spi.MetamodelImplementor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -20,5 +24,26 @@ public class HomeController {
         List<Product> listProducts = productService.listAll();
         model.addAttribute("listProducts",listProducts);
         return "index";
+    }
+
+    @RequestMapping("/new")
+    public String goNewProduct(Model model){
+        Product product = new Product();
+        model.addAttribute("product",product);
+        return "newProduct";
+    }
+
+    @RequestMapping(value="/save", method= RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product){
+        productService.save(product);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductForm(@PathVariable(name="id") Long id){
+        ModelAndView modelAndView = new ModelAndView("editProduct");
+        Product product = productService.get(id);
+        modelAndView.addObject("product",product);
+        return modelAndView;
     }
 }
